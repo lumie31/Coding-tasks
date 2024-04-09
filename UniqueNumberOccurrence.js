58d4dc3fc6a2556584a491de9dd8d56363cd4a44
Olumide Okedusi <olumideokedusi@gmail.com>
2024-04-10 00:46:32 +0300

    Add tasks description

--- UniqueNumberOccurrence.js
+++ UniqueNumberOccurrence.js
@@ -1,4 +1,8 @@
 // https://leetcode.com/problems/unique-number-of-occurrences/description/
+/*
+  Given an array of integers arr, return true if the number of occurrences of 
+  each value in the array is unique or false otherwise.
+*/
 
 var uniqueOccurrences = function (arr) {
   let obj = {};
