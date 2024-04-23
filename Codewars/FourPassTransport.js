c8b4f6dfba2128c6a0a8133b1448a5c74b15346c
Olumide <olumideokedusi@gmail.com>
2024-04-23 16:38:12 +0300

    Create FourPassTransport.js

---
+++ Codewars/FourPassTransport.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5aaa1aa8fd577723a3000049/train/javascript
+/*
+  The eccentric candy-maker, Billy Bonka, is building a new candy factory to produce his new 4-flavor sugar pops. 
+  The candy is made by placing a piece of candy base onto a conveyer belt which transports the candy through four separate processing stations in sequential order. 
+  Each station adds another layer of flavor.
+
+  Due to an error in the factory blueprints, the four stations have been constructed in incorrect locations. 
+  It's too costly to disassemble the stations, so you've been called in.
+
+  Arrange the directional path of the conveyer belt so that it passes through each of the stations in 
+  sequential order while also traveling the shortest distance possible.
+*/
+
+function fourPass(stations){
+	//your code goes here. you can do it!
+  console.log(stations)
+  
+}
